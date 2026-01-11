265a9afb9efa9009b44f09b7ebda1bd7bb497d5c
roql47 <roql47@naver.com>
2026-01-11 13:43:46 +0900

    v0.23 - 창고 기능 삭제

--- xnova-flutter/lib/presentation/screens/splash/splash_screen.dart
+++ xnova-flutter/lib/presentation/screens/splash/splash_screen.dart
@@ -468,7 +468,7 @@ class _SplashScreenState extends State<SplashScreen>
                 ),
                 const SizedBox(width: 8),
                 Text(
-                  'v0.22',
+                  'v0.23',
                   style: GoogleFonts.orbitron(
                     fontSize: 11,
                     fontWeight: FontWeight.bold,
